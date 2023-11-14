f18704b8c2c4b2898b4c75d886faa6ac221aa1a2
codeydo <63593060+codeydo@users.noreply.github.com>
2023-11-14 15:01:33 +0000

    test

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -4,12 +4,6 @@ import { CFooter } from '@coreui/react'
 const AppFooter = () => {
   return (
     <CFooter>
-      <div className="ms-auto">
-        <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          Room 4 you  &amp; Crisis managemtn Dashboard
-        </a>
-      </div>
     </CFooter>
   )
 }
