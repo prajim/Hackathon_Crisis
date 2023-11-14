7473dba4ea53801fe580c95d62da38828455d449
unknown <prajinbofo@gmail.com>
2023-11-14 15:34:03 +0000

    feat: changes

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -6,9 +6,9 @@ const AppFooter = () => {
     <CFooter>
       <div>
         <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+          Rooms 4 you
         </a>
-        <span className="ms-1">&copy; 2023 creativeLabs.</span>
+        <span className="ms-1">&copy; 2023 rooms4you.</span>
       </div>
     </CFooter>
   )
